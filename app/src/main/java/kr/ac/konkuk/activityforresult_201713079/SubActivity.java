aa5f10e3530e9cad11449e09e97c335005f7d8a3
Chanhee <chan420420@gmail.com>
2020-11-09 20:56:37 +0900

    Init & Layout

---
+++ app/src/main/java/kr/ac/konkuk/activityforresult_201713079/SubActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.konkuk.activityforresult_201713079;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+    }
+}
